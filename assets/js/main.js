2630d1ddca7939556f518de73f0962e8857d2de2
Raul Farias S <ra.farias.s@outlook.com>
2021-12-20 17:02:48 -0300

    cambios fianles vista pc
    se agrega por js que el menu cambie de color segun scroll, se agregan fuentes y se habilita el Scrollspy

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+$(() => $('[data-toggle="tooltip"]').tooltip());
+
+$(window).scroll(() => {
+  const scroll = $(window).scrollTop();
+  if (scroll >= 100) $('#menu').addClass('bg-dark-transparent');
+  else $('#menu').removeClass('bg-dark-transparent');
+});
